e23588888f2371d326d2e9d4ae96e7a727a56bcb
Raúl Molina <r.molina.2020@alumnos.urjc.es>
2026-05-31 20:24:36 +0200

    tenemosLOgin

--- chat/views.py
+++ chat/views.py
@@ -4,9 +4,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render ,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-
-
+from django.conf import settings
+from openai import OpenAI
 from .models import Conversacion,Mensaje
+
+
+def obtener_respuest_llm(mensaje_historial):
+    client = OpenAI(
+        base_url="https:///integrate.api.nvidia.com",
+        api_key=settings.NVIDIA_API_KEY
+    )
+    comletion = client.chat.completions.create(
+        model = "met/llama-3.1-8b--instruct",
+        messages= mensaje_historial,
+        temperature = 0.2,
+        max_tokens=1024,
+        stream=False
+
+    )
+    return comletion.choices[0].message.content
 def home(request):
     return render(request,"chat/home.html")
 @login_required
@@ -36,6 +52,18 @@ def chat_detalle(request,id):
                 contenido=contenido,
 
             )
+    #la api aqui
+        historial = [
+            {"role":"user"if m.rol == "User" else "assistant",
+             "content":m.contenido}
+            for m in conv.mensaje_set.order_by('-creado')
+        ]
+        respuesta = obtener_respuest_llm(historial)
+        Mensaje.objects.create(
+            conversacion = conv,
+            rol='assistant',
+            contenido=respuesta,
+        )
         return redirect('chat_detalle',id=conv.id)
     mensaje = conv.mensaje_set.order_by('-creado')
     return render(request,"chat/chat_detalle.html",{
--- chatia/settings.py
+++ chatia/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 import chatia
-
+load_dotenv()
+NVIDIA_API_KEY = os.getenv('NVIDIA_API_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
