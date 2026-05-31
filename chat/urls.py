593facec463938bb9daa3366bc78738f45fd4116
Raúl Molina <r.molina.2020@alumnos.urjc.es>
2026-05-31 17:55:33 +0200

    tenemosLOgin

--- chat/urls.py
+++ chat/urls.py
@@ -1,2 +1,5 @@
 from django.urls import path
-urlpatterns = []
+from . import views
+urlpatterns = [
+    path('chat/',views.chat_home,name='chat_home'),
+]
--- chat/views.py
+++ chat/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+@login_required
+def chat_home(request):
+    return render(request,"chat/chat_home.html")
+
