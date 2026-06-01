afd76321435deaadc2c0817fbce12c13df13a103
Raul <r.molina.2020@alumnos.urjc.es>
2026-06-01 11:02:22 +0000

    Edit tests.py

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+
+class LoginTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+    
+    def test_login_correcto(self):
+        response = self.client.post('/login/', {
+            'username': 'testuser',
+            'password': 'testpass123'
+        })
+        self.assertEqual(response.status_code, 302)
+    
+    def test_login_incorrecto(self):
+        response = self.client.post('/login/', {
+            'username': 'testuser',
+            'password': 'wrongpass'
+        })
+        self.assertEqual(response.status_code, 401)
