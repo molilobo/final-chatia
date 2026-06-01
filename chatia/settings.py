d5639766be83c65269194e3d5a8eabb6a77d208c
Raúl Molina <r.molina.2020@alumnos.urjc.es>
2026-06-01 14:50:09 +0200

    pefil_DONE

--- chatia/settings.py
+++ chatia/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 LOGIN_REDIRECT_URL = "/chat/"
 LOGOUT_REDIRECT_URL = "/login/"
 LOGIN_URL = "/login/"
