644de641e53d062ccf4ca3d1fc21fed994dcbb72
Raúl Molina <r.molina.2020@alumnos.urjc.es>
2026-06-01 11:35:06 +0200

    tenemosLOgin

--- chat/context_processor.py
+++ chat/context_processor.py
@@ -7,11 +7,11 @@ def metricas_globales(request):
     mensa_usuarios = 0
 
     if request.user.is_authenticated:
-        conv_usuarios = Conversacion.objects.filter(user=request.user).count()
-        mensa_usuarios = Mensaje.objects.filter(user=request.user).count()
+        conv_usuarios = Conversacion.objects.filter(usuario=request.user).count()
+        mensa_usuarios = Mensaje.objects.filter(conversacion__usuario=request.user).count()
     return {
             'total_conversaciones': total_conversaciones,
             'total_mensajes': total_mensajes,
-            'conv_usuarios': conv_usuarios,
-            'mensa_usuarios': mensa_usuarios,
+            'conv_usuario': conv_usuarios,
+            'mensa_usuario': mensa_usuarios,
     }
--- chatia/settings.py
+++ chatia/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                'chat.context_processors.metricas_globales',
+                'chat.context_processor.metricas_globales',
             ],
         },
     },
